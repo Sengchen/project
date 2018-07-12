c293fe1ed223dcb5aef4c0007b585347de8c9298
Christopher Jones <seng_chen@163.com>
2018-07-12 21:06:59 +0800

    thymeleaf config

---
+++ dubbo-client/src/main/java/com/example/dubboclient/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.dubboclient.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/main")
+public class MainController {
+
+	private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+	@RequestMapping(value = "")
+	public String main() {
+		log.info("main");
+		return "index";
+	}
+}
