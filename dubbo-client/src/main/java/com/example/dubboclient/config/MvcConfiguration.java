2a3e5bfd2c561d9e2d1e85146779ce8a2050a863
Christopher Jones <seng_chen@163.com>
2018-07-17 15:59:47 +0800

    add interceptor

---
+++ dubbo-client/src/main/java/com/example/dubboclient/config/MvcConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.dubboclient.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+}
